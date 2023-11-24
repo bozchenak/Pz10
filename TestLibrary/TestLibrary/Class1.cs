2fb99d05af1d2057b52c362e5f5ed34f1f663a6a
Kolycheva <bojylkaxol@mail.ru>
2023-11-24 18:01:07 +0500

    First

---
+++ TestLibrary/TestLibrary/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    public class Class1
+    {
+        int x;
+        int y;
+        public string show()
+        {
+            return $"{x} {y}";
+        }
+        public int mult()
+        { 
+            return x ^ y;
+        }
+    }
+}
